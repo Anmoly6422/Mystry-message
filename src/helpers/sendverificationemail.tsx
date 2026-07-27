7152f2c6dbd91e605d1a15521f006e37ea0dd872
Anmoly6422 <231046@kit.ac.in>
2026-07-27 12:56:57 +0530

    Switch from Resend to Nodemailer for email verification

--- src/helpers/sendverificationemail.tsx
+++ src/helpers/sendverificationemail.tsx
@@ -1,27 +1,29 @@
-import {resend} from "@/lib/resend";
-
-import VerificationEmail from "../../emails/Verificationemails";    
+import { transporter } from "@/lib/nodemailer";
+import { render } from "@react-email/render";
+import VerificationEmail from "../../emails/Verificationemails";
 import { Apiresponse } from "@/types/Apiresponse";
 
 export async function sendVerificationEmail(
-            email:string,
-            username:string,
-            verifyCode:string
-): Promise <Apiresponse>{
-    try {
-        const data = await resend.emails.send({
-  from: "onboarding@resend.dev",
-  to: email,
-  subject: "Mystry message | Verification code",
- react: <VerificationEmail username={username} otp={verifyCode} />,
-});
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<Apiresponse> {
+  try {
+    const emailHtml = await render(
+      <VerificationEmail username={username} otp={verifyCode} />
+    );
 
-console.log("RESEND RESPONSE:", data);
-         return {success:true,message:"Verificaton email send successfully"}
-    }
-    catch(emailerror){
-        console.error("Error sending verification email",
-            emailerror)
-            return {success:false,message:"Failed to send verification email"}
-    }
+    const data = await transporter.sendMail({
+      from: `"Mystery Message" <${process.env.GMAIL_USER}>`,
+      to: email,
+      subject: "Mystry message | Verification code",
+      html: emailHtml,
+    });
+
+    console.log("NODEMAILER RESPONSE:", data);
+    return { success: true, message: "Verification email sent successfully" };
+  } catch (emailerror) {
+    console.error("Error sending verification email", emailerror);
+    return { success: false, message: "Failed to send verification email" };
+  }
 }
